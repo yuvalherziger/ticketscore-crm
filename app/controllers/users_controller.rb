ea16e4e1a7e71d563758ff72d6186790eb15dff3
yuvalherziger <yuvalhrz@gmail.com>
2015-01-18 19:14:23 +0100

    Added password decryption

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,34 +1,41 @@
 class UsersController < ApplicationController
+
   def index
     @users = User.all	
   end
+
   def show
     @user = User.find(params[:id])
   end
+
   def new
     @user = User.new
   end
+
   def user_params
     params.require(:user).permit(:userName, :email, :isActive, :password)
   end
+
   def create
     @user = User.new(user_params)
 
     if @user.save
-      redirect_to users_path, :notice => "User created successfully"
+      redirect_to users_path, :notice => 'success: User created successfully'
     else
-      render "new"
+      render 'new', :notice => 'danger: Please review the errors below'
     end 
   end
+
   def edit
     @user = User.find(params[:id])
   end
+
   def update
     @user = User.find(params[:id])
-    if @post.update_attributes(user_params)
-      redirect_to users_path, :notice => "The user has been updated sucessfully"
+    if @user.update_attributes(user_params)
+      redirect_to users_path, :notice => 'success: The user has been updated sucessfully'
     else
-      render "edit", :notice => "something bad happenned" 
+      render 'edit', :notice => 'danger: something bad happenned'
     end
 	
   end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,13 @@
 class User < ActiveRecord::Base
   attr_accessor :password
+  before_save :encrypt_password
+
   validates_confirmation_of :password
   validates_presence_of :password, :on => :create
   validates_presence_of :email
+  validates_presence_of :userName
   validates_uniqueness_of :email
+  validates_uniqueness_of :userName
 
   def encrypt_password
     if password.present?
